109831591253fb7fc8ef6f5e76764f7869dd57bb
Jonathan Lee <jonathan.h.lee@gmail.com>
2018-01-17 23:30:03 -0800

    Add method to retrieve mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -16,4 +16,9 @@ class Kele
     response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
     @user_data = JSON.parse(response.body)
   end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    @availability = JSON.parse(response.body)
+  end
 end
