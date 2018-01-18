33685421e40ce5423680e8f866c3d1bbdf9a8ae5
Jonathan Lee <jonathan.h.lee@gmail.com>
2018-01-17 23:19:10 -0800

    Add method to retrieve users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -6,7 +7,13 @@ class Kele
 
   def initialize(email, password)
     response = self.class.post("/sessions", body: {"email": email, "password": password})
-    puts response
+    # puts response
+    @auth_token = response["auth_token"]
     raise 'Invalid email or password' if response.code != 200
+  end
+
+  def get_me
+    response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
   end
 end
