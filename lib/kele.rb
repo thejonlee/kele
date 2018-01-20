1aff5a2389115819a786baa94323eaa54197a492
Jonathan Lee <jonathan.h.lee@gmail.com>
2018-01-19 17:34:08 -0800

    Added get and create message methods

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,6 @@
 require 'httparty'
 require 'json'
-require relative './roadmap'
+require_relative './roadmap'
 
 class Kele
   include HTTParty
@@ -22,5 +22,26 @@ class Kele
   def get_mentor_availability(mentor_id)
     response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     @availability = JSON.parse(response.body)
+  end
+
+  def get_messages(page_number = nil)
+    if page_number == nil
+      response = self.class.get("/message_threads?1=", headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get("/message_threads?#{page_number}=", headers: { "authorization" => @auth_token })
+    end
+    @messages = JSON.parse(response.body)
+  end
+
+  def create_message(sender, recipient_id, subject, text)
+    response = self.class.post(
+      "/messages",
+      body: { "sender": sender, "recipient_id": recipient_id, "subject": subject, "stripped-text": text },
+      headers: { "authorization" => @auth_token })
+    if response.success?
+      puts "Message sent."
+    else
+      puts response
+    end
   end
 end
